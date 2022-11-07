45f63ecbc648291829eca3dbea00454bf331b233
magrega <56564097+magrega@users.noreply.github.com>
2022-11-07 16:50:29 +0300

    Adding index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Language, Genre
 
 
 def index(request):
-    return HttpResponse("Hello")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available
+    }
+
+    return render(request, 'index.html', context=context)
